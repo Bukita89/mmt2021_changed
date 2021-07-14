819ea81b98267c6d854c3b33288e86aa031f7e84
Dmitryfor <dimaportnov2000@gmail.com>
2021-07-14 10:23:44 +0300

    add blog page

--- resources/views/template-blog-page.blade.php
+++ resources/views/template-blog-page.blade.php
@@ -46,49 +46,74 @@
 
             @endif
 
-            <div class="blog-filters-wrapper">
-                
-                <p class="show-for-medium">Filter Categories</p>
+            @if ( class_exists('FacetWP') )
 
-                <div class="blog-filters">
-                    <div class="facet-dropdown-wrapper show-for-small-only">
-                        <div class="facetwp-facet facetwp-facet-resource_types facetwp-type-dropdown" data-name="resource_types" data-type="dropdown">
-                            <select class="facetwp-dropdown">
-                                <option value="">Filter Categories</option>
-                                <option value="article">Article</option>
-                                <option value="case-study">Case Study</option>
-                                <option value="video">Video</option>
-                            </select>
+                <div class="blog-filters-wrapper">
+                    
+                    <p class="show-for-medium">Filter Categories</p>
+
+                    <div class="blog-filters">
+
+                        <div class="facet-dropdown-wrapper show-for-small-only">
+
+                            {!! facetwp_display( 'facet', 'mobile_post_categories' ) !!}
+
                         </div>
+
+                        <div class="facet-radio-wrapper show-for-medium">
+
+                            {!! facetwp_display( 'facet', 'post_categories' ) !!}
+
+                        </div>
+
                     </div>
 
-                    <div class="facet-radio-wrapper show-for-medium">
-                        <div class="facetwp-facet facetwp-facet-resource_categories facetwp-type-radio" data-name="resource_categories" data-type="radio">
-                            <div class="facetwp-radio checked" data-value="all">Nutrition <span class="facetwp-counter">(1)</span></div>
-                            <div class="facetwp-radio" data-value="save-money">Workouts <span class="facetwp-counter">(2)</span></div>
-                            <div class="facetwp-radio" data-value="improve-security">Health & Wellness <span class="facetwp-counter">(3)</span></div>
-                        </div>
-                    </div>
                 </div>
-            </div>
 
-            @if ( $posts )
+            @endif
 
-                <div class="grid-x grid-margin-x block-grid medium-up-2 large-up-3 align-center template-blog-items">
+            @php
+                
+                $args = array(
+                    'post_type' => 'post',
+                    'numberposts' => 1,
+                    'status' 	=> 'publish',
+                    'fields'    => 'ids',
+                    'facetwp'   => true,
+                );
 
-                    @foreach ( $posts as $item )
+                $posts = get_posts( $args );
+
+            @endphp
+
+            @if ( !empty( $posts ) )
+
+                <div class="grid-x grid-margin-x block-grid medium-up-2 large-up-3 align-center template-blog-items facetwp-template">
+
+                    @foreach ( $posts as $post_id )
                         
                         <div class="cell">
+
                             <div class="img-wrapper">
-                                <a href="#" class="cover-link"><span class="show-for-sr">blog post link</span></a>
-                                <img src="" alt="">
+                                
+                                <a href="{!! get_permalink( $post_id ) !!}" class="cover-link"><span class="show-for-sr">blog post link</span></a>
+
+                                @if ( $image = get_the_post_thumbnail_url( $post_id ) )
+                                    <img src="{!! $image !!}" alt="{!! get_the_title( $post_id ) !!}">
+                                @endif
+
                             </div>
 
                             <div class="content">
-                                <p class="preheadline">{{preheadline}}</p>
-                                <h4 class="title">{{title}}</h4>
-                                <a href="" class="button simple arrow">Read More</a>
+
+                                <p class="preheadline"></p>
+
+                                <h4 class="title">{!! get_the_title( $post_id ) !!}</h4>
+
+                                <a href="{!! get_permalink( $post_id ) !!}" class="button simple arrow">Read More</a>
+
                             </div>
+
                         </div>
 
                     @endforeach
@@ -96,12 +121,14 @@
                 </div>
 
                 <div class="blog-pagination">
-                    <button class="button-arrow prev disabled"><span class="show-for-sr">button prev</span></button>
-                    <button class="blog-pagination__button active">1</button>
-                    <button class="blog-pagination__button">2</button>
-                    <button class="blog-pagination__button">3</button>
-                    <button class="button-arrow next"><span class="show-for-sr">button next</span></button>
+
+                    {!! facetwp_display( 'facet', 'pagination' ) !!}
+
                 </div>
+
+            @else 
+
+                <p class="text-center">Sorry, no posts were found.</p>
 
             @endif
 
