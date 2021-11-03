7fc4b9f80ea7c7802d0d6ac586f3671ad4ea30be
Victoria Naumova <nauorama@gmail.com>
2021-11-03 13:40:56 +0200

    footer updates

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,22 +2,30 @@
 
     <div class="grid-container">
 
+        <div class="contacts-bar">
+            <div class="grid-x grid-margin-x has-2-cols justify-content-between">
+                @if ( $opening_hours = get_field( 'opening_hours', 'options' ) )
+                    <div class="cell medium-shrink">
+                        <p>Hours:</p>
+                        <div class="opening-hours"><p>{!! $opening_hours !!}</p></div>
+                    </div>
+                @endif
+
+                <div class="cell medium-shrink">
+                    @if ( $physical_address = get_field( 'physical_address', 'options' ) )
+                        <div class="address"><p>{!! $builder->getAddress( $physical_address ) !!}</p></div>
+                    @endif
+
+                    @if ( $phone_number = get_field( 'primary_phone_number', 'options' ) )
+                        <div class="phone-number"><a href="tel:{!! $phone_number['e164'] !!}">{!! $builder->getPhoneNumber( $phone_number['national'] ) !!}</a></div>
+                    @endif
+                </div>
+            </div>
+        </div>
+      
         <div class="grid-x grid-margin-x">
 
             <div class="cell small-12">
-
-                {{-- @if ( $opening_hours = get_field( 'opening_hours', 'options' ) )
-                    <div class="opening-hours"><p>{!! $opening_hours !!}</p></div>
-                @endif
-
-                @if ( $physical_address = get_field( 'physical_address', 'options' ) )
-                    <div class="address"><p>{!! $builder->getAddress( $physical_address ) !!}</p></div>
-                @endif
-
-                @if ( $phone_number = get_field( 'primary_phone_number', 'options' ) )
-                    <div class="phone-number"><a href="tel:{!! $phone_number['e164'] !!}">{!! $builder->getPhoneNumber( $phone_number['national'] ) !!}</a></div>
-                @endif --}}
-
                 <div class="text-center d-flex align-items-center align-justify footer-content">
 
 					@php wp_nav_menu( $builder->getMenuArgs('footer_navigation') ); @endphp
